c6e35f8ea862c627a02b65a8c0986c0c0640e6e6
Gaurav07076 <103797867+Gaurav07076@users.noreply.github.com>
2023-12-07 19:21:19 +0530

    Skills section done

--- portfolio/src/container/Footer/Footer.jsx
+++ portfolio/src/container/Footer/Footer.jsx
@@ -1,5 +1,9 @@
-import React from 'react'
+import React,{useState} from 'react'
 import './Footer.scss'
+import {images} from '../../constants'
+import { AppWrap,MotionWrap } from '../../wrapper'
+import { client } from '../../client' 
+
 const Footer = () => {
   return (
     <div>Footer</div>
--- portfolio/src/container/Work/Work.jsx
+++ portfolio/src/container/Work/Work.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { AiFillEye, AiFillGithub } from 'react-icons/ai'
 import { motion } from 'framer-motion'
 
-import { AppWrap } from '../../wrapper';
+import { AppWrap , MotionWrap} from '../../wrapper';
 import { urlFor, client } from '../../client'
 import './Work.scss'
 
@@ -18,7 +18,7 @@ const Work = () => {
 
     client.fetch(query)
       .then((data) => {
-        console.log(data)
+       
         setWorks(data);
         setFilterWork(data);
       })
@@ -114,4 +114,7 @@ const Work = () => {
 }
 
 
-export default AppWrap(Work, 'work');
+export default AppWrap(
+  MotionWrap(Work,'app__works'),
+  'work','app__primarybg'
+);
